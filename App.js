6a2370b36f6f8be3a645a239f4e3af69dd67424d
xiaomiwujiecao <1410200695@qq.com>
2017-03-27 07:15:18 +0800

    添加了google的material design 主题

--- App.js
+++ App.js
@@ -6,41 +6,43 @@ var React = require('react'),
 
 module.exports = React.createClass({
 
-  // We initialise its state by using the `props` that were passed in when it
-  // was first rendered. We also want the button to be disabled until the
-  // component has fully mounted on the DOM
-  getInitialState: function() {
-    return {items: this.props.items, disabled: true}
-  },
+    // We initialise its state by using the `props` that were passed in when it
+    // was first rendered. We also want the button to be disabled until the
+    // component has fully mounted on the DOM
+    getInitialState: function () {
+        return {items: this.props.items, disabled: true}
+    },
 
-  // Once the component has been mounted, we can enable the button
-  componentDidMount: function() {
-    this.setState({disabled: false})
-  },
+    // Once the component has been mounted, we can enable the button
+    componentDidMount: function () {
+        this.setState({disabled: false})
+    },
 
-  // Then we just update the state whenever its clicked by adding a new item to
-  // the list - but you could imagine this being updated with the results of
-  // AJAX calls, etc
-  handleClick: function() {
-    this.setState({
-      items: this.state.items.concat('Item ' + this.state.items.length)
-    })
-  },
+    // Then we just update the state whenever its clicked by adding a new item to
+    // the list - but you could imagine this being updated with the results of
+    // AJAX calls, etc
+    handleClick: function () {
+        this.setState({
+            items: this.state.items.concat('Item ' + this.state.items.length)
+        })
+    },
 
-  // For ease of illustration, we just use the React JS methods directly
-  // (no JSX compilation needed)
-  // Note that we allow the button to be disabled initially, and then enable it
-  // when everything has loaded
-  render: function() {
-
-    return div(null,
-
-      button({onClick: this.handleClick, disabled: this.state.disabled}, 'Add Item'),
-
-      ul({children: this.state.items.map(function(item) {
-        return li(null, item)
-      })})
-
-    )
-  },
-})
+    // For ease of illustration, we just use the React JS methods directly
+    // (no JSX compilation needed)
+    // Note that we allow the button to be disabled initially, and then enable it
+    // when everything has loaded
+    render: function () {
+        return div(null,
+            button({
+                onClick: this.handleClick,
+                disabled: this.state.disabled,
+                className: this.props.defaultButton
+            }, 'Add Item'),
+            ul({
+                children: this.state.items.map(function (item) {
+                    return li(null, item)
+                })
+            })
+        )
+    },
+});
